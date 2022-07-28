781b5f6ca818f88dd0aa8a330c1b778b6608dc04
Rodrigo Casanova <rodrigocralha@gmail.com>
2022-07-28 19:52:09 +0100

    minimally useful data from the weather in metric

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import "./styles.scss";
 
 import { Weather } from "./weather";
 
-const weather = new Weather("London", "85dfcfe9ef0ce0be2a1188644d09aed0");
-//const now = weather.getWeather(); // activate in the end to not waste resources
+const info = document.querySelector(".info");
+
+const weather = new Weather("Barcelos", "85dfcfe9ef0ce0be2a1188644d09aed0");
+weather.getWeather()
+        .then(data => {
+            console.log(data)
+            info.innerHTML = `
+                <h1>${data.city}</h1>
+                <p>${data.condition.name}</p>
+                <p>${data.temperature.tmp}ºC Min:${data.temperature.min}ºC Max:${data.temperature.max}ºC</p>
+                <p>${data.humidity.value}%</p>
+            `;
+        }); // activate in the end to not waste resources
 
--- src/weather.js
+++ src/weather.js
@@ -4,41 +4,48 @@ export class Weather{
         this.city = city;
         this.apiKey = apiKey;
     }
+
+    changeCity(city)
+    {
+        this.city = city;
+    }
+
     async getWeather()
     {
         // geocoding
         const coords = await this.getCityCoords();
 
         // fetching
-        const url = `http://api.openweathermap.org/data/2.5/forecast?appid=${this.apiKey}&lat=${coords.lat}&lon=${coords.lon}`;
+        const url = `http://api.openweathermap.org/data/2.5/forecast?appid=${this.apiKey}&lat=${coords.lat}&lon=${coords.lon}&units=metric`;
         const response = await fetch( url, { mode : 'cors' });
         const data = await response.json();
-        
+
+        console.log(data);
+
         const weather = {
             'city': this.city,
             'temperature': {
-                'unit': data.temperature.unit,
-                'value': data.temperature.value,
-                'min': data.temperature.min,
-                'max': data.temperature.max
+                'tmp': Number(data.list[0].main.temp),
+                'min': Number(data.list[0].main.temp_min),
+                'max': Number(data.list[0].main.temp_max)
             },
             'humidity': {
-                'unit': data.humidity.unit,
-                'value': data.humidity.value
+                'value': data.list[0].main.humidity
             },
             'condition': {
-                'name': data.weather.value,
-                'icon': data.condition.icon
+                'name': data.list[0].weather[0].description,
+                'icon': data.list[0].weather[0].icon
             }
         }
         
         return weather;
     }
+
     async getCityCoords()
     {
         const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${this.city}&limit=2&appid=${this.apiKey}`;
         const response = await fetch(geoUrl);
         const data = await response.json();
-        return { 'lat': data[0].lat, lon: data[0].lon };
+        return { 'lat': data[0].lat, 'lon': data[0].lon };
     }
 }
